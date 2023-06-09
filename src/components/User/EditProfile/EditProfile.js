829da48348a15ed9c138165a4c3847d65c53a2b5
Bui Tri Thuc <buitrithuc1008@gmail.com>
2023-06-09 09:58:07 +0700

    update editprofile

--- src/components/User/EditProfile/EditProfile.js
+++ src/components/User/EditProfile/EditProfile.js
@@ -1,21 +1,36 @@
 import "./EditProfile.css";
 import React from "react";
 import EditProfileModal from "./EditProfileModal";
+import { Link } from "react-router-dom";
 
 export default function EditProfile() {
   return (
-    <div>
-      <div className="editProfile">
+    <div className="ads">
+      <div className="editProfile_all">
         <div className="editProfile_top">
           <EditProfileModal />
         </div>
         <div className="editProfile_center">
           <div className="editProfile_center_input">
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
+            <div>Tên</div>
+            <input className="editprofile_input" type="text" />
+            <div>Mô hình kinh doanh </div>
+            <input className="editprofile_input" type="text" />
+            <div>Số điện thoại</div>
+            <input className="editprofile_input" type="text" />
+            <div>Địa chỉ</div>
+            <input className="editprofile_input" type="text" />
           </div>
+        </div>
+        <div className="editprofile_button">
+          <Link to="/">
+            {" "}
+            <button className="editprofile_button_cancel">Thoát</button>
+          </Link>
+          <Link to="/profile">
+            {" "}
+            <button className="editprofile_button_save">Lưu</button>
+          </Link>
         </div>
       </div>
     </div>
