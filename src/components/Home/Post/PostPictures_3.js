af83053a4387011ccbbf46c4258739f67ffc5cf0
TinIT28 <tinmcit@gmail.com>
2023-06-10 13:08:10 +0700

    Fix bug css

--- src/components/Home/Post/PostPictures_3.js
+++ src/components/Home/Post/PostPictures_3.js
@@ -15,7 +15,7 @@ export default function PostPictures3({ imgSrc, postId }) {
                   ? {
                       width: "100%",
                       height: "200px",
-                      border: "1.5px solid #5C5C60",
+                      border: "0.25px solid #5C5C60",
                     }
                   : {}
               }
